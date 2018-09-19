b0290e97bc001a1cfa36a994b000902cd954487a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 20:13:05 -0700

    add functions

--- calculator.py
+++ calculator.py
@@ -14,13 +14,27 @@ def my_calculator():
 		split_input = raw_input.split(" ")
 
 		for i in range(1,len(split_input)):
-			split_input[i] = int(split_input[i])
+			split_input[i] = float(split_input[i])
 
 		if split_input[0] == "q":
 			break
 		else:
 			if split_input[0] == "+":
 				print (add(split_input[1],split_input[2]))
+			elif split_input[0] == "-":
+				print (subtract(split_input[1], split_input[2]))
+			elif split_input[0] == "*":
+				print (multiply(split_input[1], split_input[2]))
+			elif split_input[0] == "/":
+				print (divide(split_input[1], split_input[2]))				
+			elif split_input[0] == "square":
+				print (square(split_input[1]))
+			elif split_input[0] == "cube":
+				print (cube(split_input[1]))
+			elif split_input[0] == "pow":
+				print (power(split_input[1], split_input[2]))
+			elif split_input[0] == "mod":
+				print (mod(split_input[1], split_input[2]))
 
 my_calculator()
 #     read input
